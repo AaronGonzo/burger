d2ac0aeea6be37a5a93c5cb718721a054ba0a1fe
Gonzo <aarongonzo31@gmail.com>
2019-05-21 22:03:49 -0700

    final commit

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Arsenal3190",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burger_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
